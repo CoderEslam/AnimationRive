3875304ab0f0ed7d9e442b97b20377a7a93f7189
CoderEslam <eslamghazy600@gmail.com>
2023-08-05 20:57:04 +0200

    Initial Commit

---
+++ AnimationRive/AnimationRiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationRiveApp.swift
+//  AnimationRive
+//
+//  Created by Eslam Ghazy on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationRiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationRive/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AnimationRive
+//
+//  Created by Eslam Ghazy on 5/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
